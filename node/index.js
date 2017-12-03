096d33c98403000f89317415f6340e6a5cd387a0
Anthony Dickerson <deoism@gmail.com>
2017-12-03 13:49:13 -0500

    node with handlebars and html

--- node/index.js
+++ node/index.js
@@ -1,15 +1,66 @@
 const http = require('http');
-
-const hostname = 'localhost';
+const express = require('express');
+const fs = require("fs");
+const hostname = "localhost";
 const port = 3000;
+const app = express();
 
-const server = http.createServer((request,response) =>{
-    response.statusCode = 200
-    response.setHeader('Content-type', 'text/plain');
-    response.end('welcome to the world of node development \n');
+const handlebars = require("express-handlebars")
+.create({ defaultLayout: 'main' });
 
+app.engine('handlebars', handlebars.engine);
+
+app.set('view engine', 'handlebars');
+
+app.set('port',process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+    // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+    console.log(__dirname+ '/index.html');
+  
+  })
+  app.get('/begining', (req, res) => {
+    res.sendFile(__dirname + '/begining.html')
+    // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+  
+  })
+    app.get('/middle', (req, res) => {
+    res.sendFile(__dirname + '/middle.html')
+    // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+  
+  })
+    app.get('/end', (req, res) => {
+    res.sendFile(__dirname + '/end.html')
+    // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+  
+  })
+     console.log(__dirname);
+
+app.get('/home', (request, response) => {
+    response.render('layouts/home');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+app.get('/about', (request, response) => {
+    response.render('layouts/about');
+}); 
+
+app.use((request, response) => {
+    response.status(404);
+    response.render('layouts/404');
+});
+
+
+
+app.listen(app.get("port"),()=>{
+    console.log(
+        "Express started on http://localhost:" +
+        app.get("port") +
+        "; press Ctrl-C to terminate. "
+    );
+});
+  
---
+++ node/views/main.js
@@ -0,0 +1,7 @@
+
+<a href="/home">home</a>||
+<a href="/about">About</a>||
+<a href="/main">main</a>||
+<a href="/begining">begining of story</a>||
+<a href="/middle">middle of story</a>||
+<a href="/end">end of story</a>||
